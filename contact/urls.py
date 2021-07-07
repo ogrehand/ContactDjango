c626608a5d0a8f37161be746a409401e184f4071
tobings <tobingsetyawan@gmail.com>
2021-07-07 18:26:19 +0700

    chnaging url pattern to plural form

--- contact/urls.py
+++ contact/urls.py
@@ -3,7 +3,7 @@ from contact import views
 
 
 urlpatterns = [
-    path('person/', views.PersonList.as_view()),
+    path('persons/', views.PersonList.as_view()),
     path('contacts/', views.ContacsOnlyList.as_view()),
     path('contacts/<int:PersonId>/', views.ContactsList.as_view()),
     # path('contactslist/<str:PersonId>/', views.ContactsList.as_view()),
